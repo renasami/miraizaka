9339b9d6e81444e034bc9560bedee663e08889e2
renasami <drrr0502@gmail.com>
2021-12-12 21:00:25 +0900

    editing sidebar

--- front/src/components/organisms/Sidebar.tsx
+++ front/src/components/organisms/Sidebar.tsx
@@ -1,11 +1,29 @@
 import React from 'react';
-
+import SidebarItem from '../molecules/SidebarItem'
+import List from "@material-ui/core/List";
+import HomeIcon from "@material-ui/icons/Home";
+import AccountBalanceIcon from '@mui/icons-material/AccountBalance';
+import AccountBoxIcon from '@mui/icons-material/AccountBox';
 
 type Props {
 
 }
 const SideBar:React.FC<Props> = (props:Props) => {
-
+    const listProps = [
+        {
+            "className":"",
+            "icon":<HomeIcon />,
+            "itemText":"",
+            "to":"",
+            "onClick":""
+        }
+    ]
+    return (
+        <>
+            <List>
+            </List>
+        </>
+    )
 }
 
 export default SideBar;
