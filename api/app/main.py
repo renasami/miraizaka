e2d5c7296c8c7ad2d4807ffb0868655babb23b47
Kuroi_Cc <ChenchengMiKuRo@gmail.com>
2021-12-13 12:07:18 +0900

    テストapi

--- api/app/main.py
+++ api/app/main.py
@@ -1,23 +1,24 @@
-from fastapi import FastAPI,Body,status
+from fastapi import FastAPI, status
 from fastapi.responses import JSONResponse
+from typing import List
+
+from app.schema import FaceSchema
 
 app = FastAPI()
 
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return {"Hello": "World!"}
 
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str = None):
     return {"item_id": item_id, "q": q}
 
-@app.post("/test_post")
-def test_post():
-    
-    return JSONResponse(status_code=status.HTTP_200_CREATED)
 
-@app.post("/test")
-def test():
-    return True
+@app.post("/test_post")
+def test_post(face_list: List[FaceSchema]):
+    for i in face_list:
+        print(i.json())
+    return JSONResponse(status_code=status.HTTP_200_OK)
