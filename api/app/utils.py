ce82988d0d3be93c36fe02d1892ac59218df8315
Kuroi_Cc <ChenchengMiKuRo@gmail.com>
2021-12-14 21:16:59 +0900

    encodeとdecodeの関数化

---
+++ api/app/utils.py
@@ -0,0 +1,13 @@
+import cv2
+import numpy as np
+
+import base64
+
+
+def encode_img_to_base64(img):
+    _, encing = cv2.imencode(".jpg", img)
+    return base64.b64encode(encing.tostring()).decode("utf-8")
+
+
+def decode_img_base64(img_base64):
+    return cv2.imdecode(np.fromstring(base64.b64decode(img_base64), dtype="uint8"), 1)
