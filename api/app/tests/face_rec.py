3f247028e5df66db92f13f7a1eaa9de550cb421a
renasami <drrr0502@gmail.com>
2022-01-08 03:54:37 +0900

    pre commit

--- api/app/tests/face_rec.py
+++ api/app/tests/face_rec.py
@@ -7,9 +7,12 @@ from torch.autograd import Variable
 print("start program")
 
 test_img = "./1a001.png"
-loader = transforms.Compose([transforms.Resize(299),
+loader = transforms.Compose([
+    transforms.Resize(299),
       transforms.CenterCrop(299),
-      transforms.ToTensor()])
+      transforms.ToTensor(),
+      transforms.Normalize(mean=[0.484,0.456,0.406],std=[0.229,0.224,0.225])
+    ])
 vgg = models.vgg16(pretrained=True)
 mlp = nn.Sequential(
     nn.Linear(1000,200),
@@ -22,21 +25,24 @@ model = nn.Sequential(
     vgg,
     mlp
 )
-model = model.load_state_dict(torch.load("./miraizaka_vgg.pth"))
 
-# model.eval()
+
+net = model.load_state_dict(torch.load("./miraizaka_vgg.pth"))
+print(type(model))
+model.eval()
 
 
 def img_loader(path):
     img = Image.open(path)
+    img.thumbnail(500,500)
     img = loader(img)
-    img = Variable(img, requires_grad=True)
+    # img = Variable(img, requires_grad=True)
     img = img.unsqueeze(0)
     return img
 
 m = nn.Softmax(dim=1)
 img = img_loader(test_img)
-print(m(model(img)))
+print(model(img))
 
 
 #python face_rec.py
