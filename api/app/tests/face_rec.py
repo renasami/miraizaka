ad10e5c582214f542b7bd0a3965d1369d98efe17
renasami <drrr0502@gmail.com>
2022-01-08 04:43:27 +0900

    今日はもう限界です

--- api/app/tests/face_rec.py
+++ api/app/tests/face_rec.py
@@ -8,10 +8,10 @@ print("start program")
 
 test_img = "./1a001.png"
 loader = transforms.Compose([
-    transforms.Resize(299),
+      transforms.Resize(299),
       transforms.CenterCrop(299),
       transforms.ToTensor(),
-      transforms.Normalize(mean=[0.484,0.456,0.406],std=[0.229,0.224,0.225])
+      transforms.Normalize([0.5], [0.5])
     ])
 vgg = models.vgg16(pretrained=True)
 mlp = nn.Sequential(
@@ -26,22 +26,22 @@ model = nn.Sequential(
     mlp
 )
 
-
+#!重要
 net = model.load_state_dict(torch.load("./miraizaka_vgg.pth"))
 print(type(model))
 model.eval()
+#!重要ここまで
 
 
 def img_loader(path):
     img = Image.open(path)
-    img.thumbnail(500,500)
-    img = loader(img)
+    # img = loader(img)
     # img = Variable(img, requires_grad=True)
-    img = img.unsqueeze(0)
+    # img = img.unsqueeze(0)
     return img
 
-m = nn.Softmax(dim=1)
 img = img_loader(test_img)
+print(img)
 print(model(img))
 
 
