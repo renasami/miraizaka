721c20d8e58fe46792df543a6e77ff7ec00c99f5
Kuroi_Cc <ChenchengMiKuRo@gmail.com>
2022-01-12 11:59:54 +0900

    add-userエンドポイントを追加、テスト用エンドポイントを別ファイルに置いた

--- api/app/main.py
+++ api/app/main.py
@@ -1,35 +1,54 @@
-from fastapi import FastAPI, UploadFile, status, File
-from fastapi.responses import JSONResponse
+import requests
+import logging
+
+from fastapi import FastAPI, Depends
+from fastapi import Request, Response
+from starlette.middleware.cors import CORSMiddleware
+
 import cv2  # noqa
 import numpy as np  # noqa
+from sqlalchemy.orm import Session
 
-from typing import List
-import requests
-
-from face_ee_manager.schema import HTTPFace, HTTPFacePack
-from face_ee_manager import decode_img  # noqa
+from face_ee_manager.schema import HTTPFace, HTTPFacePack  # noqa: F401
+from face_ee_manager import decode_img  # noqa: F401
 from app import config
+from app.db.session import session
+from app.utils import get_db
+from app import crud
+from app.crud.schemas.user import UserCreate
+from app.test_router import router as test_router
 
 app = FastAPI(title=config.PROJECT_NAME)
+app.include_router(test_router, prefix="/test", tags=["test"])
+logger = logging.getLogger("uvicorn.error")
+
+
+@app.middleware("http")
+async def db_session_middleware(request: Request, call_next):
+    response: Response = Response("Internal server error", status_code=500)
+    try:
+        request.state.db = session()
+        logger.info("Get DB session")
+        response = await call_next(request)
+
+    finally:
+        request.state.db.close()
+        logger.info("Close DB session")
+    return response
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
 def read_root():
     return {"Hello": "World!"}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str = None):
-    return {"item_id": item_id, "q": q}
-
-
-@app.post("/test_post")
-def test_post(x, y, w, h, cw, ch, file: UploadFile = File(...)):
-    print("Testing")
-    print(x, y, w, h, cw, ch)
-    print(file.read())
-
-    return JSONResponse(content=file.filename, status_code=status.HTTP_200_OK)
 
 
 @app.post("/notify")
@@ -49,25 +68,11 @@ def notify(state: int):
     return response.status_code
 
 
-@app.post("/test")
-def test_p(face_list: List[HTTPFace]):
-    size_li = []
-    for i in face_list:
-        size_li.append({"file_size": len(i.img_base64)})
-        print(len(i.img_base64), type(i.img_base64))
-        # img = decode_img(i.img_base64)
-        # img = img[:, :, ::-1]
-        # cv2.imwrite("/app/test1.jpg", img)
-    return JSONResponse(content=size_li, status_code=status.HTTP_200_OK)
-
-
-@app.post("/test_async")
-def test_async(face_pack: HTTPFacePack):
-    print(face_pack.index)
-    print(face_pack.total)
-    return face_pack
-
-
 @app.post("/recognize")
 def recognize():
     return 1
+
+
+@app.post("/add-user")
+def add_user(user_in: UserCreate, db: Session = Depends(get_db)):
+    return crud.user.create(db, obj_in=user_in)
